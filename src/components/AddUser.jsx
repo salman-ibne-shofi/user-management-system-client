60151f62db84d36a6445d0a6b9c952b564193ce5
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-11-28 18:58:11 +0600

    created add user form and get value successfully from client site

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,7 +1,54 @@
 const AddUser = () => {
+	const handleAddUser = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const email = form.email.value;
+		const password = form.password.value;
+		console.log(email, password);
+	};
 	return (
 		<div>
-			<h2>new user</h2>
+			<div className="hero bg-base-200 min-h-screen">
+				<div className="hero-content flex-col">
+					<div className="text-center lg:text-center">
+						<h1 className="text-5xl font-bold">New User</h1>
+						<p>Use the below form to create a new account</p>
+					</div>
+					<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+						<form onSubmit={handleAddUser} className="card-body">
+							<div className="form-control">
+								<label className="label">
+									<span className="label-text">Email</span>
+								</label>
+								<input
+									type="email"
+									name="email"
+									placeholder="email"
+									className="input input-bordered"
+									required
+								/>
+							</div>
+							<div className="form-control">
+								<label className="label">
+									<span className="label-text">Password</span>
+								</label>
+								<input
+									type="password"
+									name="password"
+									placeholder="password"
+									className="input input-bordered"
+									required
+								/>
+							</div>
+							<div className="form-control mt-6">
+								<button className="btn btn-primary">
+									Add User
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
